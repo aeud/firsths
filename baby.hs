7d07738791fad8deaed700a0bfdf1bcb7e32cefe
aeud <adrien.eudes@gmail.com>
2014-12-23 17:47:57 +0800

    Improvements

--- baby.hs
+++ baby.hs
@@ -1,1 +1,7 @@
-doubleMe x = x + x  
+doubleMe x = x + x
+doubleSmallNumber x = (if x < 100 then 2*x else x)
+factorial :: (Integral a) => a -> a  
+factorial 0 = 1  
+factorial n = n * factorial (n - 1)
+
+fn = ceiling . negate . tan . cos . max 50  
